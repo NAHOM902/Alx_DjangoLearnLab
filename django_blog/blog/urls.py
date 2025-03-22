d7b8652538792a9f510e0340192ad59efe9c8f2e
NAHOM902 <nahombelayneh387>
2025-03-22 16:02:25 +0300

    added the post management features and files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
 
     #Blog Post URLs
     path('posts/', BlogPostListView.as_view(), name="post-list"),
-    path("posts/<int:pk>/", BlogPostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/", BlogPostDetailView.as_view(), name="post-detail"),
     path("posts/new", BlogPostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit", BlogPostEditView.as_view(), name="post-edit"),
+    path("post/<int:pk>/update", BlogPostEditView.as_view(), name="post-edit"),
     path("posts/<int:pk>/delete", BlogPostDeleteView.as_view(), name="post-delete"),
 ]
