b4dabed679d2d0a5898fa86a8fb7280921cb5aef
NAHOM902 <nahombelayneh387>
2025-03-22 16:00:08 +0300

    added the post management features and files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from . import views
-
+from .views import  (RegisterView, ProfileView, LoginView, LogoutView,
+                      BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostEditView,
+                      BlogPostDeleteView
+                      )
 
 urlpatterns = [
 
     #Authentication URLs
-    path('register/', views.RegisterView, name="register"),
-    path('login/', views.LoginView, name="login"),
-    path('logout/', views.LogoutView, name="logout"),
-    path('profile/', views.ProfileView, name="profile")
+    path('register/', RegisterView, name="register"),
+    path('login/', LoginView, name="login"),
+    path('logout/', LogoutView, name="logout"),
+    path('profile/', ProfileView, name="profile"),
+
+    #Blog Post URLs
+    path('posts/', BlogPostListView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", BlogPostDetailView.as_view(), name="post-detail"),
+    path("posts/new", BlogPostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/edit", BlogPostEditView.as_view(), name="post-edit"),
+    path("posts/<int:pk>/delete", BlogPostDeleteView.as_view(), name="post-delete"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,56 @@ from django.contrib import messages
 from .forms import RegisterForm
 
 
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+    #Blog post Detail, update, delete views
+
+class BlogPostCreateView( LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/create.html'
+    fields = ['author', 'title', 'content']
+    
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def get_success_url(self):
+        # Redirect to the list of blog posts after successful creation
+        return reverse_lazy('post-list')
+  
+
+
+class BlogPostListView(ListView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/list_view.html'
+
+class BlogPostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/detail_view.html'
+
+class BlogPostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/edit.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class BlogPostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/delete.html'
+    success_url = reverse_lazy('post-list')
 
 
     # Register view
@@ -25,7 +75,7 @@ def RegisterView(request):
     return render(request, "blog/register.html", {'form': form})
 
 
-    #Profile view
+    #login view
 
 def LoginView(request):
     if request.method == "POST":
@@ -44,8 +94,12 @@ def LoginView(request):
     return render(request, "blog/login.html", {'form': form})
 
 
+    #profile view
+
 def ProfileView(request):
     return render(request, 'blog/profile.html')
+
+    #logout view
 
 def LogoutView(request):
     logout(request)
