86420c7015f792bcf9390d1b82004b28dd5ab8eb
NAHOM902 <nahombelayneh387>
2025-03-23 12:44:28 +0300

    added the tag and search functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
+from .models import Comment, Post
 
 
 
@@ -27,3 +27,31 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+
+
+
+from taggit.forms import TagWidget  # Import TagWidget if using django-taggit for tags
+
+# Post creation and update form with custom widgets
+class PostForm(forms.ModelForm):
+    # Custom widget for the 'tags' field, if using django-taggit
+    tags = forms.CharField(
+        widget=TagWidget(),  # Uses the TagWidget for tag input
+        required=False
+    )
+    
+    # You can also customize other fields if necessary
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))  # Custom textarea widget
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include tags in the form fields
+
+class CustomTextWidget(forms.Textarea):
+    def __init__(self, attrs=None):
+        # You can add custom attributes for the widget here
+        if attrs is None:
+            attrs = {}
+        attrs.update({'class': 'custom-text-widget', 'placeholder': 'Enter text here...'})
+        super().__init__(attrs)
